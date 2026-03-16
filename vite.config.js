20995461e29e97a2f7026657f58f117ec85b0329
Joel Dic <joeldic@Joels-MacBook-Air.local>
2026-03-16 11:42:51 -0400

    chore: set Vite root to src/ and remove root index.html (src is canonical)

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 
+// Use `src/` as canonical source root and output production files to `dist/` at repo root
 export default defineConfig({
+  root: 'src',
   build: {
-    outDir: 'dist'
+    outDir: '../dist'
   }
 })
